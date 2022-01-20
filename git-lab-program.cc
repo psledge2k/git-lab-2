a895cec02ec944f227895921cb697364ebee7bf7
psledge2k <97693671+psledge2k@users.noreply.github.com>
2022-01-19 19:58:05 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Pierre Sledge
+ *        Date: 1/19/2022
+ * Description: Difficulty 10
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+cout << "Hello Git!!" << endl;
+    return 0;
+  g++ -Wall git-lab-program.cc;
+  ./a.exe;
+}// main
